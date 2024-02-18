0511ade355dfc99b7968e05a12e1b9ae4fbcd919
Alves0611 <g.alves0611@gmail.com>
2024-02-18 17:02:57 -0300

    imported cognito

---
+++ imports.tf
@@ -0,0 +1,14 @@
+# import {
+#   to = aws_cognito_user_pool.this
+#   id = "us-east-1_kyTShKH1w"
+# }
+
+# import {
+#   to = aws_cognito_user_pool_client.this
+#   id = "us-east-1_kyTShKH1w/1vjmh7e0u3h0kgmak5tb1s37h0"
+# }
+
+# import {
+#   to = aws_cognito_user_pool_domain.this
+#   id = "api-todos"
+# }
