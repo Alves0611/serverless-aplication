09f7a87368093fabbc83f6e7fe4fd6df7938c5e1
Alves0611 <g.alves0611@gmail.com>
2024-02-18 17:06:26 -0300

    dinamodb configuration created

--- locals.tf
+++ locals.tf
@@ -3,4 +3,11 @@ locals {
   namespaced_service_name = "${var.service_name}-${var.environment}"
 
   has_domain_name = var.domain_name != null
+
+  dynamodb_config = {
+    partition_key = "TodoId"
+    sort_key      = "UserId"
+    gsi_name      = "TodosByUser"
+    gsi_sort_key  = "Done"
+  }
 }
